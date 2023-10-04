0a0abf2118d4e45d7010c7a48fb5d300ecf132f3
naiym <naiymhoshen@gmail.com>
2023-10-04 21:08:25 +0600

    Initial commit

--- lib/Pages/home_pages.dart
+++ lib/Pages/home_pages.dart
@@ -10,6 +10,15 @@ class HomePages extends StatefulWidget {
 }
 
 class _HomePagesState extends State<HomePages> {
+  List<String>? nameList;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    nameList=[];
+    nameList?.add!("Md.Naim Hosen");
+    nameList?.add!("Md.Nahid Hosen");
+  }
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -30,38 +39,132 @@ class _HomePagesState extends State<HomePages> {
           centerTitle: true,
         ),
         drawer: DrawerPages(),
-        body:  Padding(
+        body: Padding(
           padding: EdgeInsets.all(10),
           child: Column(
             children: [
               Row(
                 children: [
-                  Icon(Icons.menu_book,size:20,),
-                  SizedBox(width:2,),
-                  Text("Hello ",style: TextStyle(fontSize:15,fontWeight: FontWeight.normal),),
-                  SizedBox(width:2,),
-                  Text("MD.Naim Hosen ",style: TextStyle(fontSize: 15,fontWeight: FontWeight.normal),),
+                  Icon(
+                    Icons.menu_book,
+                    size: 20,
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  Text(
+                    "Hello ",
+                    style:
+                        TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  Text(
+                    "MD.Naim Hosen ",
+                    style:
+                        TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+                  ),
                   // SizedBox(width:2,),
                   // Text("Good Morning ",style: TextStyle(fontSize: 15,fontWeight: FontWeight.normal),)
                 ],
               ),
-              SizedBox(height: 10,),
-              Container(
-                child:TextField(
-                  onChanged: (value){},
-                  decoration: InputDecoration(
-                    hintText: "Search by name",hintStyle: TextStyle(color: Colors.black12),
-                    fillColor: Colors.blue.shade300,
-                    filled:true,
-                    focusedBorder: UnderlineInputBorder(
-                       borderSide: BorderSide(
-                         color: Colors.black12
-                       ),
-                      borderRadius: BorderRadius.all(Radius.circular(7))
-                    )
-                  ),
-                ) ,
+              SizedBox(
+                height: 10,
               ),
+              Container(
+                child: TextField(
+                  onChanged: (value) {},
+                  decoration: InputDecoration(
+                      hintText: "Search by name",
+                      hintStyle: TextStyle(color: Colors.white),
+                      fillColor: Colors.white,
+                      filled: true,
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black12),
+                          borderRadius: BorderRadius.all(Radius.circular(10)))),
+                ),
+              ),
+              SizedBox(height: 5,),
+              Expanded(
+                  child: ListView.separated(
+                      separatorBuilder: (context, index) {
+                        return SizedBox(
+                          height: 6,
+                        );
+                      },
+                      itemCount:nameList!.length,
+                      itemBuilder: (
+                        context,
+                        index,
+                      ) {
+                        String name=nameList![index];
+                        return Padding(
+                          padding: EdgeInsets.only(left: 5, right: 5),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                color: Colors.white,
+                                border: Border.all(
+                                    width: 1, color: Colors.black26)),
+                            child: Padding(
+                              padding: EdgeInsets.all(6),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                      child: Column(
+                                    children: [
+                                      Text(
+                                      nameList![index],
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 15),
+                                      ),
+                                      Text(
+                                        "Here is my content",
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.normal,
+                                            fontSize: 15),
+                                      ),
+                                      Text(
+                                        "03 October 2023",
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.normal,
+                                            fontSize: 15),
+                                      ),
+                                    ],
+                                  ),
+                                  ),
+                                  PopupMenuButton(
+                                    padding:EdgeInsets.zero,
+                                      onSelected: (value){},
+                                      icon: Icon(Icons.more_vert),
+                                      itemBuilder: (context){
+                                      return [
+                                        PopupMenuItem(
+                                            value: "edit",
+                                            child: ListTile(
+                                          leading: Icon(Icons.edit),
+                                          title: Text("Update"),
+                                        )),
+                                        const PopupMenuItem(
+                                            value: "Delete",
+                                            child: ListTile(
+                                          leading: Icon(Icons.delete),
+                                          title: Text("Delete"),
+                                        ))
+                                      ];
+
+                                  })
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      }))
             ],
           ),
         ),
